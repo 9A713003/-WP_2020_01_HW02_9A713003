f2b595f5c1b76220f41bb52dff9ff55c7a40d6ed
9A713003 <72925023+9A713003@users.noreply.github.com>
2020-11-18 22:35:58 +0800

    Add files via upload

---
+++ WP_2020_01_HW02_9A713003/WP_2020_01_HW02_9A713003/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WP_2020_01_HW02_9A713003
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Poker p = new Poker();
+            List<int> poker = p.GetPoker(13);
+            
+
+                string msg = "";
+            for (int i = 0; i < poker.Count; i++)
+            {
+                msg = $"{poker[i]} ,";
+            }
+
+                richTextBox1.Text = $"黑桃{ msg}" ;
+            }
+
+        }
+    }
+
